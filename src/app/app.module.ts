49df2e089db742a4fa85ad34e60425eff40b7857
Kubatov Tamás <kubatovtamas01@gmail.com>
2021-05-05 09:20:48 +0200

    Add initial routing config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialComponentsModule } from './material-components/material-components.module';
-import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { AddAppointmentComponent } from './add-appointment/add-appointment.component';
+import { ListAppointmentsComponent } from './list-appointments/list-appointments.component';
+import { RouterModule, Routes } from '@angular/router';
+import { EditAppointmentComponent } from './edit-appointment/edit-appointment.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'list', component: ListAppointmentsComponent },
+  { path: 'add', component: AddAppointmentComponent },
+  { path: 'edit/:id', component: EditAppointmentComponent },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddAppointmentComponent,
+    ListAppointmentsComponent,
+    EditAppointmentComponent
   ],
   imports: [
     BrowserModule,
     RouterModule,
     BrowserAnimationsModule,
     NoopAnimationsModule,
-    MaterialComponentsModule
+    MaterialComponentsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
